eb1777b265592fdb12a3933400feaec98f1d27b9
mrbattals <mrbattals@gmail.com>
2023-05-15 02:22:51 -0500

    new assiggments added

--- lottery/Main.java
+++ lottery/Main.java
@@ -1,87 +1,33 @@
 package lottery;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-    private static boolean[] lottery;
-    private static boolean[] array;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int winnerNumber = randomNumber100();
+		int[] userNumbers5 = new int [5];
+		int userNumber1 = randomNumber25();
 
-    public static void main(String[] args) {
+	}
+		public static int randomNumber25(){
+		double random = Math.random() * 26;
+		return (int) random; 
+		
+	}
+	
+		public static int randomNumber75(){
+		double random = Math.random() * 76;
+		return (int) random;
+	}
+		public static int randomNumber100(){
+			double random = Math.random() * 101;
+			return (int) random;
+		}
 
-        getUserNumbers();
-
-        getRandomNumbers();
-
-        getTotalMatchedNumbers();
-
-}
+	
+	}
 
 
-    public static void getRandomNumbers(){
 
-            int[] lottery = new int[6];
-            int lotteryNumbers;
-        
-            for (int i = 0; i < 6; i++) {
-                lotteryNumbers = (int) (Math.random() * 50);
-                for (int x = 0; x < i; x++) {
-                    if (lottery[x] == lotteryNumbers)
-                    {
-                        lotteryNumbers = (int) (Math.random() * 50);
-                        x = -1;
-                    }
-        
-                }
-                lottery[i] = lotteryNumbers;
-            }
-            
-            System.out.println("The winning numbers are: ");
-            for (int i = 0; i < lottery.length; i++)
-                System.out.print(lottery[i] + " ");
-        
-        }
-    
-
-    public static void getUserNumbers(){
-        Scanner scanner = new Scanner(System.in);
-         
-            int[] array = new int[10];  
-    
-        System.out.println("Enter your numbers: "); 
-    
-        for(int i=0; i < 6; i++){  
-      
-            array[i]=scanner.nextInt();  
-    
-        }  
-
-        System.out.println("You have entered: ");  
-
-        for (int i=0; i < 6; i++){  
-            System.out.print(array[i]);  
-            System.out.print(" ");
-            System.out.println(" ");
-           
-        }  
-    }
-
-    public static void getTotalMatchedNumbers(){
-
-        int matched = 0;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0;j < 6; j++){
-                if(lottery[i] == array[j]){
-                        matched = matched + 1;
-                }
-            }
-            if (matched != 6){
-                System.out.println(matched + " out of the 6 numbers you chose are winning numbers, better luck next time!");
-            }
-            else{
-                System.out.println("You got picked all the winning numbers! You win!");
-            }
-        }
-
-    }
-}
